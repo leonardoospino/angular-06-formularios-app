a3ecbb9596dc66b0501d25f118df68066abd0206
Leonardo F. Ospino Ayala <leonardo.ospino.ayala@gmail.com>
2021-09-08 12:43:18 -0500

    Validar número mayor o igual a 0

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -17,13 +17,22 @@ export class BasicosComponent implements OnInit {
 
   nombreValido(): boolean {
     return (
-      this.miFormulario?.controls.producto?.invalid &&
-      this.miFormulario?.controls.producto?.touched
+      this.miFormulario?.controls.producto?.touched &&
+      this.miFormulario?.controls.precio?.value < 0
+    );
+  }
+
+  precioValido(): boolean {
+    console.log('asdasd');
+    return (
+      this.miFormulario?.controls.precio?.invalid &&
+      this.miFormulario?.controls.precio?.touched
     );
   }
 
   // guardar(miFormulario: NgForm) {
   guardar() {
     console.log(this.miFormulario.value);
+    // console.log(this.miFormulario?.controls.precio.value);
   }
 }
