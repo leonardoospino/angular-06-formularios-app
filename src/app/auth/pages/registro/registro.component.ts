a50a37486af759b8346d888454be8ffa88ae0b21
Leonardo F. Ospino Ayala <leonardo.ospino.ayala@gmail.com>
2021-09-08 21:18:12 -0500

    Evaluar correo por expresion regulra

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -10,14 +10,20 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class RegistroComponent implements OnInit {
   // TODO: Temporal
   nombreApellidoPattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+  emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
 
   miFormulario: FormGroup = this.fb.group({
-    nombre: ['', [Validators.required, Validators.pattern(this.nombreApellidoPattern)]]
+    nombre: ['', [Validators.required, Validators.pattern(this.nombreApellidoPattern)]],
+    email: ['', [Validators.required, Validators.pattern(this.emailPattern)]]
   });
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre: 'Adriana Contreras',
+      email: 'adrianaccfontalvo@gmail.com'
+    });
   }
 
   campoInvalido(campo: string) {
